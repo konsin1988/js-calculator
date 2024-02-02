294a6cd213637ccd97f79829e53f790c89af4b63
konsin1988 <konsin1988@gmail.com>
2024-02-02 14:03:29 +0300

    Fix some bugs

--- js/app.js
+++ js/app.js
@@ -8,15 +8,30 @@ const sub = document.getElementById("sub");
 const mult = document.getElementById("mult");
 const div = document.getElementById("div");
 
+function checkNumber(result) {
+  if (!result && result != 0) {
+    return "Not a number!!!";
+  } else if (!isFinite(result)) {
+    return "Division by zero";
+  } else {
+    return result;
+  }
+}
+
 add.onclick = function () {
-  result.textContent = Number(num1.value) + Number(num2.value);
+  result.textContent = checkNumber(Number(num1.value) + Number(num2.value));
 };
 sub.onclick = function () {
-  result.textContent = Number(num1.value) - Number(num2.value);
+  result.textContent = checkNumber(Number(num1.value) - Number(num2.value));
 };
 mult.onclick = function () {
-  result.textContent = Number(num1.value) * Number(num2.value);
+  result.textContent = checkNumber(Number(num1.value) * Number(num2.value));
 };
 div.onclick = function () {
-  result.textContent = (Number(num1.value) / Number(num2.value)).toFixed(5);
+  let temp = checkNumber(Number(num1.value) / Number(num2.value));
+  if (!Number.isInteger(temp)) {
+    result.textContent = temp.toFixed(5);
+  } else {
+    result.textContent = temp;
+  }
 };
